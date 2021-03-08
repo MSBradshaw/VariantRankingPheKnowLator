99b129d684f435323e97dfb60c72f7dc3a3ba367
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2021-03-08 08:53:19 -0700

    extracted communiting with walktrap and markov

--- CommunityDetection/markov.py
+++ CommunityDetection/markov.py
@@ -6,10 +6,10 @@ G = nx.read_edgelist('HPO_String_Analysis/HPO_String_edgelist.tsv', delimiter='\
 # G = nx.karate_club_graph()
 
 coms = algorithms.markov_clustering(G)
-pickle.dump(coms, open('CommunityDetection/louvain_coms.pickle', 'wb'))
+pickle.dump(coms, open('CommunityDetection/markov_coms.pickle', 'wb'))
 
 cd = coms.to_node_community_map()
 
-with open('markov.txt','w') as outfile:
+with open('CommunityDetection/markov.txt','w') as outfile:
     for key in cd.keys():
         outfile.write(key + '\t' + '\t'.join([str(x) for x in cd[key]]) + '\n')
